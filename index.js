413d13a8cd5c235a6f6eb681e70585cac7e9053f
Jake Wehder <jw@email.comjake>
2022-07-26 18:43:22 -0500

    commit tree 92b851206426b19996a56b27b75c08cd9d973416

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+
+const gifts = ['teddy bear', 'drone', 'doll']
+
+/*
+function wrapGifts(gifts) {
+    for(let gift of gifts) {
+        console.log(gift);
+    }
+}
+*/
+// outputted the correct three gifts in the array
+
+/* function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(gifts[i]);
+    }
+}
+*/
+// outputted the correct three gifts in the array
+
+/*
+function wrapGifts(gifts) {
+    for (let gift in gifts) {
+        console.log(gift);
+    }
+}
+*/
+// outputs 0, 1, & 2. This is used for objects mostly.
+
+function writeCards(name, event) {
+    const messages = [];
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    console.log(messages)
+    return messages
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function wrapGifts(gifts) {
+    i = 0;
+    while (i < gifts.length) {
+        console.log(`Wrapped ${gifts[i]}!`);
+        i++
+    }
+}
+
+wrapGifts(gifts);
+
+function countDown(integer) {
+    while (integer >= 0){
+        console.log(integer);
+        integer--;
+    }
+}
+
+// countDown(10);
